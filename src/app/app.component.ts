2cba22573cb9f3e2880979a7cbb9f1dc65ed4f23
nightblade9 <nightbladecodes@gmail.com>
2018-08-23 14:44:54 -0400

    Factory building. Ugly solution to enable/disable factory button based on resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,12 @@ export class AppComponent {
 
   title = 'Idle Sci-Fi 4X';
   playerData = PlayerData;
+  FACTORY_COST = 10;
 
+  // These elements need to be used to enable/disable buttons when we run out of resources
+  // This is painful. We have to keep state and update it appropriately.
+  // Worse yet, we can't even use !blah in our [disabled] attribute. Ouch.
+  doesntHaveEnoughResourcesForResourceAFactory = false;
   
   get constants() {
     return Constants;
@@ -20,5 +25,30 @@ export class AppComponent {
 
   public harvestResource = (index) => {
     PlayerData.resources[index] += 1;
+
+    this.updateResourcesForButtonsStates();
+
+    // This logic should probably go in a controller/service.
+    if (!PlayerData.discoveries.includes("ResourceAFactory") && this.playerHasResourcesForFactory(0))
+    {
+      PlayerData.discoveries.push("ResourceAFactory");
+    }
+  }
+
+  public buildFactory = (resourceIndex) => {
+    // redundant but safer
+    if (this.playerHasResourcesForFactory(resourceIndex)) {
+      PlayerData.resources[resourceIndex] -= this.FACTORY_COST;
+      console.log("You got a factory!"); // lulz
+      this.updateResourcesForButtonsStates();
+    }
+  }
+
+  public playerHasResourcesForFactory = (resourceIndex) => {
+    return PlayerData.resources[resourceIndex] >= this.FACTORY_COST;
+  }
+
+  private updateResourcesForButtonsStates = () => {
+    this.doesntHaveEnoughResourcesForResourceAFactory = !this.playerHasResourcesForFactory(0);
   }
 }
