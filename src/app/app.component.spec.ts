5bcb5c7c0e18abff023a7c982212530383be576c
nightblade9 <nightbladecodes@gmail.com>
2018-08-29 16:16:21 -0400

    Update unit tests. They time out, I can't seem to figure out why.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SidebarComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'idlescifi4x'`, async(() => {
+
+  it(`should have an appropriate title`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('idlescifi4x');
+    expect(app.title).toEqual('Idle Sci-Fi 4X');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render a manual harvesting section', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to idlescifi4x!');
+    expect(compiled.querySelector('#manual-harvesting p').textContent).toContain('Manual harvesting');
   }));
 });
