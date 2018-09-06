644f3fcd6636e511410a6c40aa3368e9c8c5028e
nightblade9 <nightbladecodes@gmail.com>
2018-09-06 13:18:14 -0400

    Unit tests for buildings section + resource button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,80 +33,119 @@ describe('AppComponent', () => {
   it('should have a harvest button for Resource A', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const compiled = fixture.debugElement.nativeElement;
+
     fixture.detectChanges();
     expect(compiled.querySelector('#manual-harvesting button').textContent).toContain('Harvest Cubes');
     // Fix fixture timeouts due to the use of setInterval and friends
     fixture.destroy();
   }));
 
+  it('should harvest Resource A when you click the button', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
+
+    var button = compiled.querySelector('#manual-harvesting button')
+    
+    expect(fixture.debugElement.componentInstance.playerData.resources[0]).toBe(0);
+    button.click();
+
+    expect(fixture.componentInstance.playerData.resources[0]).toBe(1);
+  }));
+
   it('should increase resources when we call harvestResource', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
 
-    fixture.componentInstance.harvestResource(0, 1);
-    expect(fixture.componentInstance.playerData.resources[0]).toEqual(1);
+    fixture.debugElement.componentInstance.harvestResource(0, 1);
+    expect(fixture.debugElement.componentInstance.playerData.resources[0]).toEqual(1);
 
-    fixture.componentInstance.harvestResource(2, 7);
-    expect(fixture.componentInstance.playerData.resources[2]).toEqual(7);
+    fixture.debugElement.componentInstance.harvestResource(1, 7);
+    expect(fixture.debugElement.componentInstance.playerData.resources[1]).toEqual(7);
 
     fixture.destroy();
   }));
 
   it('should discover Resource A factories when the player reaches 10 of resource A', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.componentInstance.playerData.resources = [0, 0];
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
 
-    fixture.componentInstance.harvestResource(0, 9);
-    expect(fixture.componentInstance.playerData.discoveries).length == 0
+    fixture.debugElement.componentInstance.harvestResource(0, 9);
+    expect(fixture.debugElement.componentInstance.playerData.discoveries).length == 0
 
-    fixture.componentInstance.harvestResource(0, 1);
-    expect(fixture.componentInstance.playerData.discoveries).length == 1;
-    expect(fixture.componentInstance.playerData.discoveries[0]).toBe("ResourceAFactory");
+    fixture.debugElement.componentInstance.harvestResource(0, 1);
+    expect(fixture.debugElement.componentInstance.playerData.discoveries).length == 1;
+    expect(fixture.debugElement.componentInstance.playerData.discoveries[0]).toBe("ResourceAFactory");
     
     // Adding more resources doesn't re-add the same thing
-    fixture.componentInstance.harvestResource(0, 86);
-    expect(fixture.componentInstance.playerData.discoveries).length == 1;
+    fixture.debugElement.componentInstance.harvestResource(0, 86);
+    expect(fixture.debugElement.componentInstance.playerData.discoveries).length == 1;
 
     fixture.destroy();
   }));
 
   it('should build a factory if the player has enough 10+ resources', async(() => {
     const fixture = TestBed.createComponent(AppComponent);    
-    fixture.componentInstance.playerData.resources = [0, 0];
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
 
-    fixture.componentInstance.harvestResource(0, 19);
+    fixture.debugElement.componentInstance.harvestResource(0, 19);
     
     // Enough to build one
-    fixture.componentInstance.buildFactory(0);
-    expect(fixture.componentInstance.playerData.factories[0]).toBe(1);
-    expect(fixture.componentInstance.playerData.resources[0]).toBe(9);
+    fixture.debugElement.componentInstance.buildFactory(0);
+    expect(fixture.debugElement.componentInstance.playerData.factories[0]).toBe(1);
+    expect(fixture.debugElement.componentInstance.playerData.resources[0]).toBe(9);
 
     // Not enough
-    fixture.componentInstance.buildFactory(0);
-    expect(fixture.componentInstance.playerData.factories[0]).toBe(1); // didn't build
-    expect(fixture.componentInstance.playerData.resources[0]).toBe(9); // didn't decrease
+    fixture.debugElement.componentInstance.buildFactory(0);
+    expect(fixture.debugElement.componentInstance.playerData.factories[0]).toBe(1); // didn't build
+    expect(fixture.debugElement.componentInstance.playerData.resources[0]).toBe(9); // didn't decrease
   }));
 
   it('should return true for playerHasResourcesForFactory if player has 10+ of that resource', async(() => {
     const fixture = TestBed.createComponent(AppComponent);    
-    fixture.componentInstance.playerData.resources = [0, 0];
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
     
-    fixture.componentInstance.harvestResource(0, 9);
-    expect(fixture.componentInstance.playerHasResourcesForFactory(0)).toBe(false)
-    fixture.componentInstance.harvestResource(0, 1);
-    expect(fixture.componentInstance.playerHasResourcesForFactory(0)).toBe(true)
+    fixture.debugElement.componentInstance.harvestResource(0, 9);
+    expect(fixture.debugElement.componentInstance.playerHasResourcesForFactory(0)).toBe(false)
+    fixture.debugElement.componentInstance.harvestResource(0, 1);
+    expect(fixture.debugElement.componentInstance.playerHasResourcesForFactory(0)).toBe(true)
   }));
 
   it('should set doesntHaveEnoughResourcesForResourceAFactory to true if we have less than 10 of resource A', async(() => {
     const fixture = TestBed.createComponent(AppComponent);    
-    fixture.componentInstance.playerData.resources = [0, 0];
+    fixture.debugElement.componentInstance.playerData.resources = [0, 0];
     
-    fixture.componentInstance.harvestResource(0, 1);
-    expect(fixture.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(true);
+    fixture.debugElement.componentInstance.harvestResource(0, 1);
+    expect(fixture.debugElement.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(true);
 
-    fixture.componentInstance.harvestResource(0, 9);
-    expect(fixture.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(false);
+    fixture.debugElement.componentInstance.harvestResource(0, 9);
+    expect(fixture.debugElement.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(false);
 
-    fixture.componentInstance.buildFactory(0)
-    expect(fixture.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(true);
+    fixture.debugElement.componentInstance.buildFactory(0)
+    expect(fixture.debugElement.componentInstance.doesntHaveEnoughResourcesForResourceAFactory).toBe(true);
+  }));
+
+  it('should show the buildings section if the player has 10+ of resource A', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('#buildings')).toBeNull();
+    fixture.debugElement.componentInstance.harvestResource(0, 10);
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('#buildings')).toBeTruthy();
+    fixture.destroy();
+  }));
+
+  it('should show the buildings section if the player has discovered resource A factories', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('#buildings')).toBeNull();
+    fixture.debugElement.componentInstance.playerData.discoveries.push("ResourceAFactory");
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('#buildings')).toBeTruthy();
+    fixture.destroy();
   }));
 });
